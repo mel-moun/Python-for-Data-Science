ec05b5d7770de9fbc73e7692fc1ce13811ad7991
mel-moun <mrymgithub@gmail.com>
2024-12-24 12:07:42 +0100

    0/ex01 done

---
+++ 0-Starting/ex01/format_ft_time.py
@@ -0,0 +1,10 @@
+import time
+import datetime
+
+curr = time.time()
+scientific = f"{curr:.4e}"
+curr_date = datetime.datetime.fromtimestamp(curr)
+date = curr_date.strftime("%b %d %Y")
+
+print(f"Seconds since January 1, 1970: {curr:,.4f} or {scientific} in scientific notation")
+print(date)
