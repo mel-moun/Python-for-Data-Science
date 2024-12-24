869ff203226e0abe61cbf3411c2452a87cfd516a
mel-moun <mrymgithub@gmail.com>
2024-12-24 11:51:02 +0100

    0/ex00 done

--- 0-Starting/ex00/Hello.py
+++ 0-Starting/ex00/Hello.py
@@ -3,6 +3,16 @@ ft_tuple = ("Hello", "toto!")
 ft_set = {"Hello", "tutu!"}
 ft_dict = {"Hello" : "titi!"}
 
+ft_list[1] = "World!"
+
+y = list(ft_tuple)
+y[1] = "France!"
+ft_tuple = tuple(y)
+
+ft_set.remove("tutu!")
+ft_set.add('Paris!')
+
+ft_dict["Hello"] = "42Paris!"
 
 print(ft_list)
 print(ft_tuple)
