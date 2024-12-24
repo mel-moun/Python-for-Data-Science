4654cd85d171dba8eb472f1ca73a52776bebce34
mel-moun <mrymgithub@gmail.com>
2024-12-24 12:33:04 +0100

    0/ex02

---
+++ 0-Starting/ex02/find_ft_type.py
@@ -0,0 +1,15 @@
+def all_thing_is_obj(object: any) -> int:
+    x = type(object)
+    if (x is list):
+        print("List :", x)
+    elif (x is tuple):
+        print("Tuple :", x)
+    elif (x is set):
+        print("Set :", x)
+    elif (x is dict):
+        print("Dict :", x)
+    elif (x is str):
+        print(object, "is in the kitchen :", x)
+    else :
+        print("Type not found")
+    return 42
