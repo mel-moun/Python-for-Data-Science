0b2dcc8036caa1aef9c0b64596e6a4ecc4d36528
mel-moun <mrymgithub@gmail.com>
2024-12-25 13:01:55 +0100

    0/ex07 done

--- 0-Starting/ex08/Loading.py
+++ 0-Starting/ex08/Loading.py
@@ -1,28 +1,25 @@
 import sys
-import time
+
 
 def ft_tqdm(lst: range) -> None:
     total = len(lst)
-    
-    
+
     i = 0
+    bar_length = 60
     for item in lst:
         progress = (i + 1) / total
-        
-        
-        bar_length = 60 
+
         filled_length = int(progress * bar_length)
         bar = '█' * filled_length + ' ' * (bar_length - filled_length)
-        percent = f'{progress*100:.0f}%'  
-        
-      
+
+        percent = f'{progress*100:.0f}%'
+
         sys.stdout.write(f'\r{percent}|{bar}| {i+1}/{total}')
         sys.stdout.flush()
-        
-      
+
         yield item
-        
+
         i += 1
-    
+
     sys.stdout.write(f'\r100%|{"█" * bar_length}| {total}/{total}\n')
     sys.stdout.flush()
