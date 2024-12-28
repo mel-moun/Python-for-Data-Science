e883979c4dd3b407fd96e976445979e5043cce97
Meriem El Mountasser <140714261+mel-moun@users.noreply.github.com>
2024-12-28 14:41:36 +0000

    ex01 done

--- 1-Array/ex01/array2D.py
+++ 1-Array/ex01/array2D.py
@@ -2,9 +2,13 @@ import numpy as np
 
 
 def slice_me(family: list, start: int, end: int) -> list:
-    family_np = np.array(family)
-    print("My shape is:", family_np.shape)
+    try:
+        family_np = np.array(family)
+        print("My shape is:", family_np.shape)
 
-    family_sl = family_np[start:end]
-    print("My new shape is:", family_sl.shape)
-    return family_sl.tolist()
+        family_sl = family_np[start:end]
+        print("My new shape is:", family_sl.shape)
+        return family_sl.tolist()
+    
+    except Exception as e:
+        print(type(e).__name__, ":", e)
