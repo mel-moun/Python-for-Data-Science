df688af89187d7ffbf41eb154780c1d5f32daec0
mel-moun <mrymgithub@gmail.com>
2024-12-28 12:13:48 +0100

    upgrade

--- 1-Array/ex00/give_bmi.py
+++ 1-Array/ex00/give_bmi.py
@@ -1,6 +1,6 @@
 from typing import Union
 
-def give_bmi(height: list[Union[int, float]], weight: list[Union[int, float]]) -> list[Union[int, float]]:
+def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
     try:
         assert len(height) == len(weight), "bad arguments"
     except AssertionError as e:
@@ -11,7 +11,7 @@ def give_bmi(height: list[Union[int, float]], weight: list[Union[int, float]]) -
         BMI.append(weight[i] / (height[i] ** 2))
     return BMI
 
-def apply_limit(bmi: list[Union[int, float]], limit: int) -> list[bool]:
+def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
     boolean = list()
     for i in range(len(bmi)):
         boolean.append(bmi[i] > limit)
