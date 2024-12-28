7d2fbfd840ecfb353d9569cce3f84a057daffcfe
Meriem El Mountasser <140714261+mel-moun@users.noreply.github.com>
2024-12-28 15:53:34 +0000

    ex02 done

---
+++ 1-Array/ex02/load_image.py
@@ -0,0 +1,20 @@
+from PIL import Image
+import numpy as np
+
+
+def ft_load(path: str) -> np.ndarray:
+    try:
+        img = Image.open(path)
+
+        if img.mode != 'RGB':
+            img = img.convert('RGB')
+
+        img_array = np.array(img)
+
+        print(f"The shape of image is: {img_array.shape}")
+
+        print(img_array)
+        return img_array
+
+    except BaseException as e:
+        print(type(e).__name__, ":", e)
