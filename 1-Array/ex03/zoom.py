94e28c5aef58d0703de011b2f8eff3956db5af90
mel-moun <mrymgithub@gmail.com>
2025-01-02 16:45:28 +0100

    ex03

--- 1-Array/ex03/zoom.py
+++ 1-Array/ex03/zoom.py
@@ -1,10 +1,23 @@
 from load_image import ft_load
-import numpy as np
+import matplotlib.pyplot as plt
+
 
 def main():
-    img = ft_load("../animal.jpeg")
-    print(img)
+    try:
+        img = ft_load("../animal.jpeg")
+        print(img)
 
-    
+        sliced_img = img[100:500, 400:800, :1]
+
+        print("New shape after slicing:", sliced_img.shape)
+        print(sliced_img)
+
+        plt.imshow(sliced_img.squeeze(), cmap='gray')
+        plt.show()
+
+    except BaseException as e:
+        print(type(e).__name__, ":", e)
+
+
 if __name__ == '__main__':
     main()
