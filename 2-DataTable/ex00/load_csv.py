7a534e6ebe3c88f8a108c937adb736aaf794df36
mel-moun <mrymgithub@gmail.com>
2025-01-04 16:18:45 +0100

    ex00 done

--- 2-DataTable/ex00/load_csv.py
+++ 2-DataTable/ex00/load_csv.py
@@ -1,11 +1,8 @@
 import pandas as pd
 
-
 def load(path: str):
     if path is None:
         return None
-    # df = pd.read_csv(path)
     df = pd.read_csv(path, index_col=0)
-    
     print("Loading dataset of dimensions ", df.shape)
     return df
