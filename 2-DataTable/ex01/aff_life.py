7d2593e0fad208f1a1ef3325595e1e5949bb635a
mel-moun <mrymgithub@gmail.com>
2025-01-05 12:38:08 +0100

    ex01...

--- 2-DataTable/ex01/aff_life.py
+++ 2-DataTable/ex01/aff_life.py
@@ -1,11 +1,22 @@
-from load_csv import load
 import matplotlib.pyplot as plt
+import pandas as pd
+from load_csv import load
 
 
 def main():
-    df = load("../life_expectancy_years.csv")
-    print(df)
+    data = load('../life_expectancy_years.csv')
 
-if __name__ == '__main__':
+    france_data = data.loc['France']
+
+    years = france_data.index.astype(int)
+    life_expectancy = france_data.values
+
+    plt.plot(years, life_expectancy)
+    plt.title('France life expectancy projections')
+    plt.xlabel('Year')
+    plt.ylabel('Life Expectancy')
+    plt.show()
+
+
+if __name__ == "__main__":
     main()
- 
