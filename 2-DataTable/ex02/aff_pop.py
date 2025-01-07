f9e08f899d8b2f7e09cd82d5ea22ebc44bf0783d
mel-moun <mrymgithub@gmail.com>
2025-01-07 10:44:32 +0100

    start ex02

--- 2-DataTable/ex02/aff_pop.py
+++ 2-DataTable/ex02/aff_pop.py
@@ -5,23 +5,20 @@ from load_csv import load
 def main():
     data = load('../life_expectancy_years.csv')
 
-    country = 'Belgium'
-    country = data.loc[country]
+    countries = ['Belgium', 'France']
 
-    years = country.index.astype(int)
-    life_expectancy = country.values
+    plt.figure()
 
-    country_two = 'France'
-    country_two = data.loc[country_two]
+    for country_name in countries:
+        country = data.loc[country_name]
+        years = country.index.astype(int)
+        life_expectancy = country.values
 
-    years_two = country_two.index.astype(int)
-    life_expectancy_two = country_two.values
+        plt.plot(years, life_expectancy, label=country_name)
 
-    plt.plot(years, life_expectancy, label=country.name, color='blue')
-    plt.plot(years_two, life_expectancy_two, label=country_two.name, color='red')
-    plt.title('Life Expectancy Comparison')
+    plt.title('Population Projections')
     plt.xlabel('Year')
-    plt.ylabel('Life Expectancy')
+    plt.ylabel('Population')
     plt.legend()
     plt.show()
 
