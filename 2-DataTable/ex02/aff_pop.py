8c705d44f5136a529e4421c12b8ef3f359366fad
mel-moun <mrymgithub@gmail.com>
2025-01-07 12:03:58 +0100

    START

--- 2-DataTable/ex02/aff_pop.py
+++ 2-DataTable/ex02/aff_pop.py
@@ -1,29 +1,23 @@
 import matplotlib.pyplot as plt
 from load_csv import load
 
+
 def main():
-    data = load('../life_expectancy_years.csv')
+    data = load('../population_total.csv')
 
-    countries = ['Belgium', 'France']
-    start_year = 1800
-    end_year = 2050
+    country = 'France'
+    country = data.loc[country]
 
-    plt.figure()
+    years = country.index.astype(int)
+    life_expectancy = country.values
 
-    for country_name in countries:
-        country = data.loc[country_name]
-        country = country.loc[str(start_year):str(end_year)]
-        years = country.index.astype(int)
-        life_expectancy = country.values
-
-        plt.plot(years, life_expectancy, label=country_name)
-
-    plt.title('Population Projections')
+    plt.plot(years, life_expectancy)
+    plt.title(str(country.name) + ' life expectancy projections')
     plt.xlabel('Year')
-    plt.ylabel('Population')
-    plt.xticks(range(start_year, end_year + 1, 40))
-    plt.legend()
+    plt.ylabel('Life Expectancy')
+    plt.legend(['Life Expectancy'])
     plt.show()
+
 
 if __name__ == "__main__":
     main()
