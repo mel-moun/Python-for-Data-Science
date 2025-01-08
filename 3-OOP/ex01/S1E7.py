53ef5691adbaa79572136555361e226a89a5f0da
mel-moun <mrymgithub@gmail.com>
2025-01-08 15:19:16 +0100

    again

--- 3-OOP/ex01/S1E7.py
+++ 3-OOP/ex01/S1E7.py
@@ -2,8 +2,9 @@ from S1E9 import Character
 
 
 class Baratheon(Character):
-    def __init__(self, first_name):
-        super().__init__(first_name)
+    def __init__(self, first_name, is_alive=True):
+        self.first_name = first_name
+        self.is_alive = is_alive
         self.family_name = 'Baratheon'
         self.eyes = 'brown'
         self.hairs = 'dark'
