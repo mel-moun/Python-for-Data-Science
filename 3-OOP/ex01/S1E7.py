9ea29113a2bb53b89bde052bfc1bfbc488a96641
mel-moun <mrymgithub@gmail.com>
2025-01-08 15:33:43 +0100

    meow

--- 3-OOP/ex01/S1E7.py
+++ 3-OOP/ex01/S1E7.py
@@ -20,4 +20,19 @@ class Baratheon(Character):
         return str((self.family_name, self.eyes, self.hairs))
 
 class Lannister(Character):
-    pass
+    """Representing the Baratheon family."""
+    def __init__(self, first_name, is_alive=True):
+        self.first_name = first_name
+        self.is_alive = is_alive
+        self.family_name = 'Lannister'
+        self.eyes = 'blue'
+        self.hairs = 'light'
+    
+    def __str__(self):
+        return str((self.family_name, self.eyes, self.hairs))
+
+    def __repr__(self):
+        return str((self.family_name, self.eyes, self.hairs))
+
+    def die(self):
+        self.is_alive = False
