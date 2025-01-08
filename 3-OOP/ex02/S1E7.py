040591474431d4bbdbcbd06b3057d42b32b6fd2d
mel-moun <mrymgithub@gmail.com>
2025-01-08 15:37:13 +0100

    tester

---
+++ 3-OOP/ex02/S1E7.py
@@ -0,0 +1,42 @@
+from S1E9 import Character
+
+
+class Baratheon(Character):
+    """Representing the Baratheon family."""
+    def __init__(self, first_name, is_alive=True):
+        self.first_name = first_name
+        self.is_alive = is_alive
+        self.family_name = 'Baratheon'
+        self.eyes = 'brown'
+        self.hairs = 'dark'
+
+    def die(self):
+        self.is_alive = False
+
+    def __str__(self):
+        return str((self.family_name, self.eyes, self.hairs))
+
+    def __repr__(self):
+        return str((self.family_name, self.eyes, self.hairs))
+
+class Lannister(Character):
+    """Representing the Baratheon family."""
+    def __init__(self, first_name, is_alive=True):
+        self.first_name = first_name
+        self.is_alive = is_alive
+        self.family_name = 'Lannister'
+        self.eyes = 'blue'
+        self.hairs = 'light'
+    
+    def __str__(self):
+        return str((self.family_name, self.eyes, self.hairs))
+
+    def __repr__(self):
+        return str((self.family_name, self.eyes, self.hairs))
+
+    def die(self):
+        self.is_alive = False
+
+    @classmethod
+    def create_lannister(self, first_name, is_alive):
+        return Lannister(first_name, is_alive)
