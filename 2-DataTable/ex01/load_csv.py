85a0786e468c39b38b19b1306cdc5e9946fe106b
mel-moun <mrymgithub@gmail.com>
2025-01-10 10:51:19 +0100

    change load

--- 2-DataTable/ex01/load_csv.py
+++ 2-DataTable/ex01/load_csv.py
@@ -2,6 +2,9 @@ import pandas as pd
 
 
 def load(path: str):
+    """
+    Loads a dataset from a CSV file and prints its dimensions.
+    """
     if path is None:
         return None
     df = pd.read_csv(path, index_col=0)
