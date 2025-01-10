cb393fffbfe3e619342845ca11be866d9f0eca23
mel-moun <mrymgithub@gmail.com>
2025-01-10 11:26:52 +0100

    dod

--- 4-Dod/ex01/in_out.py
+++ 4-Dod/ex01/in_out.py
@@ -14,16 +14,28 @@ def pow(x: int | float) -> int | float:
 
 def outer(x: int | float, function) -> object:
     """
-    Returns an inner function that applies the given
-    function to x and updates its result.
+    Returns an inner function that applies
+    the given function to x and updates its result.
     The inner function, when called, will modify
-    the value of x.
+    the value of x based on the provided function.
+    The result will be accumulated and returned each
+    time the inner function is called.
     """
-    result = x
+    count = 0
 
     def inner() -> float:
-        nonlocal result
-        result = function(result)
-        return result
+        """
+        Applies the function to the current value
+        of count (or x initially) and updates count.
+        The inner function, when called,
+        will return the updated result after applying
+        the provided function to it.
+        """
+        nonlocal count
+        if count == 0:
+            count = function(x)
+        else:
+            count = function(count)
+        return count
 
     return inner
