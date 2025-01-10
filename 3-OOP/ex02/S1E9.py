3a6da6727eac5b6f3911bbab2f3adaf51d83261f
mel-moun <mrymgithub@gmail.com>
2025-01-10 10:57:43 +0100

    S1E9

--- 3-OOP/ex02/S1E9.py
+++ 3-OOP/ex02/S1E9.py
@@ -2,26 +2,43 @@ from abc import ABC, abstractmethod
 
 
 class Character(ABC):
-    """Your docstring for Class"""
+    """
+    Abstract base class representing a character
+    with a name and life status.
+    """
 
     @abstractmethod
     def __init__(self, first_name, is_alive=True):
-        """Your docstring for Constructor"""
+        """
+        Initializes a character with a first name
+        and a default status of being alive.
+        """
         self.first_name = first_name
         self.is_alive = is_alive
 
     @abstractmethod
     def die(self):
-        """Your docstring for Method"""
+        """
+        Marks the character as no longer alive.
+        """
         pass
 
 
 class Stark(Character):
-    """Your docstring for Class"""
+    """
+    Represents a member of the Stark family,
+    inheriting from the Character class.
+    """
+
     def __init__(self, first_name, is_alive=True):
-        """Your docstring for Constructor"""
+        """
+        Initializes a Stark family member with a first name
+        and a default status of being alive.
+        """
         super().__init__(first_name, is_alive)
 
     def die(self):
-        """Your docstring for Method"""
+        """
+        Marks the Stark family member as no longer alive.
+        """
         self.is_alive = False
